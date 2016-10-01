90348bceee7e3288bdedc7254016136589aefafb
Capi Etheriel <barraponto@gmail.com>
2016-10-01 13:52:22 -0300

    [test] test creating card

--- test/services/card/index.test.js
+++ test/services/card/index.test.js
@@ -3,8 +3,24 @@
 const assert = require('assert');
 const app = require('../../../src/app');
 
+const chai = require('chai');
+chai.use(require('chai-http'));
+
 describe('card service', function() {
   it('registered the cards service', () => {
     assert.ok(app.service('cards'));
   });
+
+  it('should create a board item', (done) => {
+    chai.request(app)
+      .post('/cards')
+      .set('Accept', 'application/json')
+      .send({
+        text: 'This is a new card'
+      })
+      .end((err, res) => {
+        done();
+      });
+  });
+
 });
