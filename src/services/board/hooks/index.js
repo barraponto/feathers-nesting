fd90d07e36c8bd82114f1e798bce3e739fb0af72
Capi Etheriel <barraponto@gmail.com>
2016-10-01 14:50:54 -0300

    [service-hook] prepopulate board on save

--- src/services/board/hooks/index.js
+++ src/services/board/hooks/index.js
@@ -3,12 +3,26 @@
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 
+const card = require('../../card/card-model');
+
+const prepopulateBoard = function(hook){
+  if (hook.data.cards) {
+    // make a promise for every card in hook.data
+    const cardPromises = hook.data.cards.map(data => {
+      card.create(data)
+    });
+    // when all promises resolve, update hook.data
+    return Promise.all(cardPromises).then(cards => {
+        hook.data.cards = cards;
+    });
+  }
+};
 
 exports.before = {
   all: [],
   find: [],
   get: [],
-  create: [],
+  create: [prepopulateBoard],
   update: [],
   patch: [],
   remove: []
