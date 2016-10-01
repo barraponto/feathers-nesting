e2a93f5d6f5d840addb9b2aa91f611f8e60b0dad
Capi Etheriel <barraponto@gmail.com>
2016-09-30 21:12:14 -0300

    feathers generate service card

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const card = require('./card');
 const board = require('./board');
 const authentication = require('./authentication');
 const user = require('./user');
@@ -12,4 +13,5 @@ module.exports = function() {
   app.configure(authentication);
   app.configure(user);
   app.configure(board);
+  app.configure(card);
 };
