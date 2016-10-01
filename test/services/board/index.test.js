05fd5564fa360c8e945995e1d370cc233edeb9a4
Capi Etheriel <barraponto@gmail.com>
2016-10-01 01:56:56 -0300

    [test] create a board

--- test/services/board/index.test.js
+++ test/services/board/index.test.js
@@ -3,8 +3,23 @@
 const assert = require('assert');
 const app = require('../../../src/app');
 
+const chai = require('chai');
+chai.use(require('chai-http'));
+
 describe('board service', function() {
-  it('registered the boards service', () => {
-    assert.ok(app.service('boards'));
-  });
+    it('registered the boards service', () => {
+        assert.ok(app.service('boards'));
+    });
+    it('should create a board item', (done) => {
+        chai.request(app)
+            .post('/boards')
+            .set('Accept', 'application/json')
+            .send({
+                text: 'This is a new board'
+            })
+            .end((err, res) => {
+                console.log(res.body);
+                done();
+            });
+    });
 });
